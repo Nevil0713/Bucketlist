d635cb0143c3c26316dbee214ac6f7b712f538b0
rapidoluna <rapidoluna22@gmail.com>
2025-07-14 16:30:44 +0900

    Tet_Stage Control

--- Assets/Scripts/rapidoluna/Script/Tet_Stage.cs
+++ Assets/Scripts/rapidoluna/Script/Tet_Stage.cs
@@ -15,26 +15,79 @@ public class Tet_Stage : MonoBehaviour
     public int boardWidth = 10;
     [Range(5, 20)]
     public int boardHeight = 20;
-    public float fallCycle = 1.0f;
+    public float fallCycle = 0.5f;
 
     private int halfWidth;
     private int halfHeight;
+
+    private float nextFallTime;
 
     private void Start()
     {
         halfWidth = (int)(boardWidth * 0.5f);
         halfHeight = (int)(boardHeight * 0.5f);
 
+        nextFallTime = Time.time + fallCycle;
+
         CreateBackground();
+        CreateBlock();
     }
 
+    void Update()
+    {
+        Vector3 moveDir = Vector3.zero;
+        bool isRotate = false;
+
+        //player control
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            moveDir.x = -0.5f;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            moveDir.x = 0.5f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            isRotate = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            moveDir.y = -0.5f;
+        }
+        
+        //fall automatically
+        if (Time.time > nextFallTime)
+        {
+            nextFallTime = Time.time + fallCycle;
+            moveDir = Vector3.down;
+            isRotate = false;
+        }
+
+        if (moveDir != Vector3.zero || isRotate)
+        {
+            moveTeto(moveDir, isRotate);
+        }
+    }
+
+    bool moveTeto(Vector3 moveDir, bool isRotate)
+    {
+        tet_blockNode.transform.position += moveDir;
+        if (isRotate)
+        {
+            tet_blockNode.transform.rotation *= Quaternion.Euler(0, 0, 90);
+        }
+
+        return true;
+    }
 
     //Creates Tile code
     Tile CreateTile(Transform parent, Vector2 position, Color color, int order = 1)
     {
         var go = Instantiate(tilePrefab);
         go.transform.parent = parent;
-        go.transform.localPosition = position * 0.55f;
+        go.transform.localPosition = position * 0.5f;
 
         var tile = go.GetComponent<Tile>();
         tile.color = color;
@@ -46,7 +99,7 @@ public class Tet_Stage : MonoBehaviour
     //Creates BG
     void CreateBackground()
     {
-        Color color = Color.grey;
+        Color color = Color.gray;
 
         //board
         color.a = 0.8f;
@@ -72,4 +125,96 @@ public class Tet_Stage : MonoBehaviour
             CreateTile(backgroundNode, new Vector2(x, -halfHeight), color, 0);
         }
     }
+
+    //creates tet_block
+    void CreateBlock()
+    {
+        int index = Random.Range(0, 7);
+        Color32 color = Color.white;
+
+        tet_blockNode.rotation = Quaternion.identity;
+        tet_blockNode.position = new Vector2(0, halfHeight - 1);
+
+        switch (index)
+        {
+            //I 모양
+            case 0:
+                color = new Color32(115, 251, 253, 255);
+
+                CreateTile(tet_blockNode, new Vector2(-2f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(-1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 0f), color);
+
+                break;
+
+            //J 모양
+            case 1:
+                color = new Color32(0, 33, 245, 255);
+
+                CreateTile(tet_blockNode, new Vector2(-1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(-1f, 1f), color);
+
+                break;
+
+            //L 모양
+            case 2:
+                color = new Color32(243, 168, 59, 255);
+
+                CreateTile(tet_blockNode, new Vector2(-1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 1f), color);
+
+                break;
+
+            //O 모양
+            case 3:
+                color = new Color32(255, 253, 84, 255);
+
+                CreateTile(tet_blockNode, new Vector2(0f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 1f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 1f), color);
+
+                break;
+
+            //S 모양
+            case 4:
+                color = new Color32(117, 250, 76, 255);
+
+                CreateTile(tet_blockNode, new Vector2(-1f, -1f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, -1f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 0f), color);
+
+                break;
+
+            //T 모양
+            case 5:
+                color = new Color32(155, 47, 246, 255);
+
+                CreateTile(tet_blockNode, new Vector2(-1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 1f), color);
+                
+                break;
+
+            //Z 모양
+            case 6:
+                color = new Color32(235, 51, 35, 255);
+
+                CreateTile(tet_blockNode, new Vector2(-1f, 1f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 1f), color);
+                CreateTile(tet_blockNode, new Vector2(0f, 0f), color);
+                CreateTile(tet_blockNode, new Vector2(1f, 0f), color);
+                
+                break;
+        }
+
+    }
+
 }
