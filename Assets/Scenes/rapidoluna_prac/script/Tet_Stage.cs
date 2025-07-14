304bbf0ab21513321a76d088e5f34c7dfefd46e6
rapidoluna <rapidoluna22@gmail.com>
2025-07-14 12:08:19 +0900

    Tet_Stage
    테트리스 스테이지 준비 중

--- Assets/Scenes/rapidoluna_prac/script/Tet_Stage.cs
+++ Assets/Scenes/rapidoluna_prac/script/Tet_Stage.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class BG_Script : MonoBehaviour
+public class Tet_Stage : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
